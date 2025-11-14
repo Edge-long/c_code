e0a5891c26b9f13e1816209ae7bf473d0779dc5c
Edge-long <15584481120m@sina.cn>
2025-11-14 13:55:54 +0800

    Add files via upload

---
+++ practices3.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+
+int main03()
+{
+	//每年各月份的天数
+	//输入时间年和月，计算当年当月的天数，通过函数实现
+
+	int year = 0;
+	int month = 0;
+	scanf("%d%*c%d", &year, &month);
+	int days = get_days_of_month(year, month);
+	printf("%d\n", days);
+
+	return 0;
+}
+
+//计算输入年输入月的天数
+int get_days_of_month(int y, int m)
+{
+	//默认存储平年每月天数
+	int arr[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	if (is_leap_year(y) && m == 2)
+	{
+		//是闰年修改二月天数
+		++arr[m - 1];
+	}
+	return arr[m - 1];
+}
+
+//判断输入年是否为闰年
+int is_leap_year(int y)
+{
+	//能被4整除同时不能被100整除或能被400整除
+	if ((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0))
+	{
+		return true;
+	}
+	return false;
+}
