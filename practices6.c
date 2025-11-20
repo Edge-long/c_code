46c2102e0b3bb2701570028fc4d779afd3f6221c
Edge-long <15584481120m@sina.cn>
2025-11-20 10:47:41 +0800

    Add files via upload

---
+++ practices6.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//求第n个斐波那契数的函数
+int Fib(int n)
+{
+	int a = 1;
+	int b = 1;
+	//默认c为第1或第2个斐波那契数
+	int c = 1;
+	//如果第n个斐波那契数不是第1或2个
+	while (n > 2)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		n--;
+	}
+	return c;
+}
+//int Fib(int n)
+//{
+//	//递到第1和2个斐波那契数
+//	if (n <= 2)
+//	{
+//		//第1和2个斐波那契数在数列中为1
+//		return 1;
+//	}
+//	else
+//	{
+//		return Fib(n - 1) + Fib(n - 2);
+//	}
+//}
+
+int main06()
+{
+	//求第n个斐波那契数
+
+	int n = 0;
+	scanf("%d", &n);
+	int r = Fib(n);
+	printf("%d\n", r);
+
+	return 0;
+}
