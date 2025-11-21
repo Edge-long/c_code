fdd58fb25a4a1b9f12b36050dd4bb71ad31ce97a
Edge-long <15584481120m@sina.cn>
2025-11-21 13:35:33 +0800

    Add files via upload

---
+++ exercises11.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main11()
+{
+	//11. ⾃幂数
+	//11.1 题⽬描述：
+	//写⼀个代码打印1~100000之间的所有的⾃幂数，中间⽤空格分隔。
+
+	for (int i = 1; i <= 100000; i++)
+	{
+		//记录i的位数
+		int n = 0;
+		int temp = i;
+		//计算i的位数
+		while (temp)
+		{
+			n++;
+			temp /= 10;
+		}
+		//记录i每一位数的i的位数幂的和
+		int sum = 0;
+		temp = i;
+		//计算i每一位数的i的位数幂的和
+		while (temp)
+		{
+			sum += (int)pow(temp % 10, n);
+			temp /= 10;
+		}
+		if (sum == i)
+		{
+			//i为自幂数打印
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+
+	return 0;
+}
