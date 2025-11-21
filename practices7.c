35cadf1762f94898556091489dbd026afcf8caed
Edge-long <15584481120m@sina.cn>
2025-11-21 21:05:27 +0800

    Add files via upload

---
+++ practices7.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main07()
+{
+	//不能创建第三个变量，实现两个整数的交换
+
+	//方法一
+	int a = 0;
+	int b = 0;
+	scanf("%d%*c%d", &a, &b);
+	//打印交换前的值
+	printf("a = %d, b = %d\n", a, b);
+	//a等于a与b的和
+	a = a + b;
+	//a减b等于原a
+	b = a - b;
+	//a减新b等于原b
+	a = a - b;
+	//打印交换后的值
+	printf("a = %d, b = %d\n", a, b);
+
+	//方法二
+	scanf("%d%*c%d", &a, &b);
+	//打印交换前的值
+	printf("a = %d, b = %d\n", a, b);
+	//利用异或操作符特性
+	//0 ^ 3 = 3; -> 3 ^ 3 = 0; -> 3 ^ 3 ^ 5 = 5; -> 3 ^ 5 ^ 3 = 0;
+	a = a ^ b;
+	b = a ^ b; //a ^ b ^ b = a
+	a = a ^ b; //a ^ b ^ a = b
+	//打印交换后的值
+	printf("a = %d, b = %d\n", a, b);
+
+	return 0;
+}
