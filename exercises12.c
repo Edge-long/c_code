278c811069b6fc959741796f5aa4f8694ac94527
Edge-long <15584481120m@sina.cn>
2025-11-23 22:22:13 +0800

    Add files via upload

---
+++ exercises12.c
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main12()
+{
+	//12. 打印菱形
+	//12.1 题⽬描述：
+	//输⼊⼀个整数n，打印对应2 * n - 1⾏的菱形图案
+
+	int n = 0;
+	scanf("%d", &n);
+	//先打印上n - 1行，再打印下n行
+	//控制上菱形打印(2 * n - 1 = n - 1 + n)
+	for (int i = 0; i < n - 1; i++)
+	{
+		//控制一行打印的空格(n - 1至1)
+		for (int j = 0; j < n - 1 - i; j++)
+		{
+			printf(" ");
+		}
+		//控制一行打印的符号(1至1 + 2 * (n - 2))
+		for (int j = 0; j < 1 + 2 * i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	//控制下菱形打印(2 * n - 1 = n - 1 + n)
+	for (int i = 0; i < n; i++)
+	{
+		//控制一行打印的空格(0至n - 1)
+		for (int j = 0; j < i; j++)
+		{
+			printf(" ");
+		}
+		//控制一行打印的符号(2 * n - 1至1)
+		for (int j = 0; j < 2 * n - 1 - i * 2; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
---
+++ practices8.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main08()
+{
+	//求一个整数存储在内存中的二进制中1的个数
+
+	//方法一
+	int num = 0;
+	//记录输入值的二进制形式中1的个数
+	int count = 0;
+	scanf("%d", &num);
+	for (int i = 0; i < 32; i++)
+	{
+		if (((num >> i) & 1) == 1)
+		{
+			count++;
+		}
+	}
+	printf("%d\n", count);
+
+	//方法二
+	int n = 0;
+	//记录输入值的二进制形式中1的个数
+	int c = 0;
+	scanf("%d", &n);
+	while (n)
+	{
+		//每次遍历时让n的二进制形式减一个1的个数
+		n = n & (n - 1);
+		c++;
+	}
+	printf("%d\n", c);
+
+	return 0;
+}
