8d3b5a9296948cbde4c25695aa52f06c5a17aadf
Edge-long <15584481120m@sina.cn>
2025-11-30 21:10:19 +0800

    Add files via upload

---
+++ exercises15.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//交换两个整数的函数
+void Swap(int* a, int* b)
+{
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
+
+int main15()
+{
+	//15. 交换两个整数
+	//15.1 题⽬描述：
+	//写⼀个函数Swap，可以交换两个整数的内容。注意必须实现成函数完成。
+	//输⼊2个整数，调⽤⾃定义函数Swap，完成2个整数的交换。
+
+	int a = 0;
+	int b = 0;
+	scanf("%d%*c%d", &a, &b);
+	Swap(&a, &b);
+	printf("%d %d", a, b);
+
+	return 0;
+}
